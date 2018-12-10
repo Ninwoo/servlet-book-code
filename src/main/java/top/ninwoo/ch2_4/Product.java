893f9564f5cfac2b8caaec5423b8568e528528ee
joliu <ljo0412@live.com>
2018-12-10 09:36:46 +0800

    第二章源码 主要包括以下部分： * Url重写 * Post隐藏域 * HttpSession

---
+++ src/main/java/top/ninwoo/ch2_4/Product.java
@@ -0,0 +1,52 @@
+package top.ninwoo.ch2_4;
+
+/**
+ * @Description:
+ * @Author: ninwoo
+ * @Date: 2018/12/10 0:30
+ */
+public class Product {
+    private int id;
+    private String name;
+    private String description;
+    private float price;
+
+    public Product(int id, String name, String description, float price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
---
+++ src/main/java/top/ninwoo/ch2_4/ShoppingItem.java
@@ -0,0 +1,32 @@
+package top.ninwoo.ch2_4;
+
+/**
+ * @Description:
+ * @Author: ninwoo
+ * @Date: 2018/12/10 0:31
+ */
+public class ShoppingItem {
+    private Product product;
+    private int quantity;
+
+    public ShoppingItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
